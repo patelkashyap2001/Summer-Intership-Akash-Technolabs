ead5cdc8dd8d533fe7e2e26449d360e459db83eb
Patel Kashyap <90511091+patelkashyap2001@users.noreply.github.com>
2022-06-18 19:23:08 +0530

    practical python intership
    Day 2 Progarmme

--- Helloword.py
+++ Helloword.py
@@ -1,1 +1,38 @@
-print ('Hello' , 'Word')
+# print ('Hello' , 'Word')
+
+'''
+Data types:- 
+
+n1=10
+n2=10.5
+n3=10+5j
+print("n1 is ",n1)
+print("n2 is ",n2)
+print("n3 is",n3)
+print(isinstance(n1,int))
+
+name="Kashyap Patel"
+print("value is",name)
+print(type(name))
+print(name[1:4])
+
+List datatype:- 
+l1=[10,20.5,30,'abc',30.5]
+print(l1)
+
+tuple Datatype:- 
+
+
+Dictionary Datatype:-
+
+
+d1={1:100,10.5:200,'abc':20.5}
+print(d1)
+print(d1[1])
+
+'''
+d={1:'Akash',2:'Twchnolbs','key':10}
+print(type(d))
+print("d[1]=",d[1])
+print("d[2]=",d[2])
+print("d['key']=",d['key'])
